c8123cee5b9aac0470f8de390d46ce8d0ca9e594
Antoine Duprat <aduprat@linagora.com>
2016-09-13 10:36:45 +0200

    MAILET-115 Extract SpecialAddress from AbstractRedirect

--- server/mailet/mailets/src/test/java/org/apache/james/transport/mailets/redirect/NotifySenderTest.java
+++ server/mailet/mailets/src/test/java/org/apache/james/transport/mailets/redirect/NotifySenderTest.java
@@ -28,8 +28,6 @@ import java.net.UnknownHostException;
 import javax.mail.MessagingException;
 
 import org.apache.james.dnsservice.api.DNSService;
-import org.apache.james.transport.mailets.redirect.NotifySender;
-import org.apache.james.transport.mailets.redirect.AbstractRedirect.SpecialAddress;
 import org.apache.mailet.MailAddress;
 import org.apache.mailet.base.test.FakeMailContext;
 import org.apache.mailet.base.test.FakeMailetConfig;
