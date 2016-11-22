b22c287e32b77df99b2e0558bd91f70b4bee5024
Antoine Duprat <aduprat@apache.org>
2016-11-22 11:17:03 +0100

    fixup! MAILET-115 Extract arrayToString from GenericMailet

--- mailet/base/src/main/java/org/apache/mailet/base/StringUtils.java
+++ mailet/base/src/main/java/org/apache/mailet/base/StringUtils.java
@@ -23,6 +23,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
+import com.google.common.base.Joiner;
+
 /**
  * Collects useful string utility methods.
  */
@@ -117,12 +119,7 @@ public final class StringUtils {
         }
         StringBuilder sb = new StringBuilder(1024);
         sb.append("[");
-        for (int i = 0; i < array.length; i++) {
-            if (i > 0) {
-                sb.append(",");
-            }
-            sb.append(array[i]);
-        }
+        sb.append(Joiner.on(",").join(array));
         sb.append("]");
         return sb.toString();
     }
