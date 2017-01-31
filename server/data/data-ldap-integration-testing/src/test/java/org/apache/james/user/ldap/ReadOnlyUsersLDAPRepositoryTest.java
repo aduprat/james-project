9d1e1e7e6617eef7589d76c36d9608aa965105ea
Antoine Duprat <aduprat@linagora.com>
2017-01-31 14:40:25 +0100

    JAMES-1922 Introduce LDAP usersRepository tests

---
+++ server/data/data-ldap-integration-testing/src/test/java/org/apache/james/user/ldap/ReadOnlyUsersLDAPRepositoryTest.java
@@ -0,0 +1,91 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.user.ldap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.plist.PropertyListConfiguration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReadOnlyUsersLDAPRepositoryTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadOnlyUsersLDAPRepositoryTest.class);
+    private static final String DOMAIN = "james.org";
+    private static final String PASSWORD = "mysecretpassword";
+
+    private LdapGenericContainer ldapContainer = new LdapGenericContainer(DOMAIN, PASSWORD);
+    private ReadOnlyUsersLDAPRepository ldapRepository;
+
+    @Before
+    public void setup() throws Exception {
+        ldapContainer.start();
+        ldapRepository = new ReadOnlyUsersLDAPRepository();
+        ldapRepository.configure(ldapRepositoryConfiguration());
+        ldapRepository.setLog(LOGGER);
+        ldapRepository.init();
+    }
+
+    private HierarchicalConfiguration ldapRepositoryConfiguration() throws ConfigurationException {
+        PropertyListConfiguration configuration = new PropertyListConfiguration();
+        configuration.addProperty("[@ldapHost]", ldapHost());
+        configuration.addProperty("[@principal]", "cn=admin\\,dc=james\\,dc=org");
+        configuration.addProperty("[@credentials]", PASSWORD);
+        configuration.addProperty("[@userBase]", "ou=People\\,dc=james\\,dc=org");
+        configuration.addProperty("[@userIdAttribute]", "uid");
+        configuration.addProperty("[@userObjectClass]", "inetOrgPerson");
+        configuration.addProperty("[@maxRetries]", "4");
+        configuration.addProperty("[@retryStartInterval]", "0");
+        configuration.addProperty("[@retryMaxInterval]", "8");
+        configuration.addProperty("[@retryIntervalScale]", "1000");
+        return configuration;
+    }
+
+    private String ldapHost() {
+        return "ldap://" +
+                ldapContainer.getContainerIpAddress() +
+                ":" + 
+                ldapContainer.getMappedPort(LdapGenericContainer.DEFAULT_LDAP_PORT);
+    }
+
+    @After
+    public void tearDown() {
+        ldapContainer.stop();
+    }
+
+    @Test
+    public void knownUserShouldBeAbleToLogInWhenPasswordIsCorrect() throws Exception {
+        assertThat(ldapRepository.test("james-user", "secret")).isTrue();
+    }
+
+    @Test
+    public void knownUserShouldNotBeAbleToLogInWhenPasswordIsNotCorrect() throws Exception {
+        assertThat(ldapRepository.test("james-user", "badpassword")).isFalse();
+    }
+
+    @Test
+    public void unknownUserShouldNotBeAbleToLogIn() throws Exception {
+        assertThat(ldapRepository.test("unknown", "badpassword")).isFalse();
+    }
+}
