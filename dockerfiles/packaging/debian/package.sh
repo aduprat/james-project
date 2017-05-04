f53327f20e64fab6ee03267609de15ca0f9f1d46
Antoine Duprat <aduprat@linagora.com>
2017-05-04 11:42:45 +0200

    fixup! JAMES-2010 Introduce debian packaging

--- dockerfiles/packaging/debian/package.sh
+++ dockerfiles/packaging/debian/package.sh
@@ -2,23 +2,25 @@
 
 printUsage() {
    echo "Usage : "
-   echo "./package.sh ITERATION"
+   echo "./package.sh RELEASE ITERATION"
+   echo "    RELEASE  : The release to be generated."
    echo "    ITERATION: The iteration to give to the package."
    exit 1
 }
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     printUsage
 fi
 
-ITERATION=$1
+RELEASE=$1
+ITERATION=$2
 cp /jars/james-server.jar /debian/package/usr/share/james/
 cp -r /jars/james-server-cassandra-guice.lib /debian/package/usr/share/james/
 cp /jars/james-cli.jar /debian/package/usr/share/james/
 
 fpm -s dir -t deb \
  -n james \
- -v 3.0-beta6 \
+ -v $RELEASE \
  -a x86_64 \
  -d openjdk-8-jre \
  -C package \
