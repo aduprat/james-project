d813ce62c03d38c4d0e8a92b6e36236ec45e58bb
benwa <btellier@linagora.com>
2017-09-27 16:32:09 +0700

    fixup! MAILBOX-307 Mailbox ACL JSON representation SHOULD be explicit

--- mailbox/api/src/main/java/org/apache/james/mailbox/model/MailboxACL.java
+++ mailbox/api/src/main/java/org/apache/james/mailbox/model/MailboxACL.java
@@ -185,14 +185,6 @@ public class MailboxACL {
             this.value = EnumSet.of(Right.forChar(right.asCharacter()));
         }
 
-        /* Used for json serialization (probably a bad idea) */
-        public Rfc4314Rights(int serializedRights) {
-            this(copyOf(Right.allRights
-                .stream()
-                .filter(right -> ((serializedRights >> right.ordinal()) & 1) != 0)
-                .collect(Collectors.toList())));
-        }
-
         public Rfc4314Rights(String serializedRfc4314Rights) throws UnsupportedRightException {
             this.value = copyOf(serializedRfc4314Rights.chars()
                 .mapToObj(i -> (char) i)
