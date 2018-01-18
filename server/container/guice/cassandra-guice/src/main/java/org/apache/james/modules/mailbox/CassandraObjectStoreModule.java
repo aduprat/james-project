43dc40a88706da5c54e2624cc4d5afb369b02e21
Antoine Duprat <aduprat@linagora.com>
2018-01-18 14:55:51 +0100

    fixup! JAMES-2287 Guice injections for object store

--- server/container/guice/cassandra-guice/src/main/java/org/apache/james/modules/mailbox/CassandraObjectStoreModule.java
+++ server/container/guice/cassandra-guice/src/main/java/org/apache/james/modules/mailbox/CassandraObjectStoreModule.java
@@ -19,7 +19,9 @@
 
 package org.apache.james.modules.mailbox;
 
+import org.apache.james.blob.api.BlobId;
 import org.apache.james.blob.api.ObjectStore;
+import org.apache.james.blob.cassandra.CassandraBlobId;
 import org.apache.james.blob.cassandra.CassandraBlobsDAO;
 
 import com.google.inject.AbstractModule;
@@ -29,7 +31,9 @@ public class CassandraObjectStoreModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(CassandraBlobsDAO.class).in(Scopes.SINGLETON);
+        bind(CassandraBlobId.Factory.class).in(Scopes.SINGLETON);
 
         bind(ObjectStore.class).to(CassandraBlobsDAO.class);
+        bind(BlobId.Factory.class).to(CassandraBlobId.Factory.class);
     }
 }
