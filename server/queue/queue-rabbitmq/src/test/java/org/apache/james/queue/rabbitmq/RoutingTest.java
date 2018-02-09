e1f3406098098bb62eac668e697e658171e4076f
benwa <btellier@linagora.com>
2018-02-09 09:44:29 +0700

    fixup! JAMES-2334 Demonstrate that routing is working

--- server/queue/queue-rabbitmq/src/test/java/org/apache/james/queue/rabbitmq/RoutingTest.java
+++ server/queue/queue-rabbitmq/src/test/java/org/apache/james/queue/rabbitmq/RoutingTest.java
@@ -19,6 +19,11 @@
 
 package org.apache.james.queue.rabbitmq;
 
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.DIRECT;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.DURABLE;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.EXCHANGE_NAME;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.NO_PROPERTIES;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.awaitAtMostOneMinute;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.nio.charset.StandardCharsets;
@@ -27,21 +32,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.rabbitmq.client.AMQP;
+import com.google.common.collect.Iterables;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 @ExtendWith(DockerRabbitMQExtension.class)
 public class RoutingTest {
-
-    private static final String EXCHANGE_NAME = "exchangeName";
-    private static final boolean DURABLE = true;
-    private static final AMQP.BasicProperties NO_PROPERTIES = null;
     private static final String CONVERSATION_1 = "c1";
     private static final String CONVERSATION_2 = "c2";
     private static final String CONVERSATION_3 = "c3";
     private static final String CONVERSATION_4 = "c4";
+
     private ConnectionFactory connectionFactory1;
     private ConnectionFactory connectionFactory2;
     private ConnectionFactory connectionFactory3;
@@ -49,10 +51,10 @@ public class RoutingTest {
 
     @BeforeEach
     public void setup(DockerRabbitMQ rabbitMQ) {
-        connectionFactory1 = RabbitMQUtils.prepareConnectionFactory(rabbitMQ);
-        connectionFactory2 = RabbitMQUtils.prepareConnectionFactory(rabbitMQ);
-        connectionFactory3 = RabbitMQUtils.prepareConnectionFactory(rabbitMQ);
-        connectionFactory4 = RabbitMQUtils.prepareConnectionFactory(rabbitMQ);
+        connectionFactory1 = rabbitMQ.connectionFactory();
+        connectionFactory2 = rabbitMQ.connectionFactory();
+        connectionFactory3 = rabbitMQ.connectionFactory();
+        connectionFactory4 = rabbitMQ.connectionFactory();
     }
 
     @Test
@@ -67,7 +69,7 @@ public class RoutingTest {
              Channel channel4 = connection4.createChannel()) {
 
             // Declare the exchange and a single queue attached to it.
-            channel1.exchangeDeclare(EXCHANGE_NAME, "direct", DURABLE);
+            channel1.exchangeDeclare(EXCHANGE_NAME, DIRECT, DURABLE);
 
             String queue1 = channel1.queueDeclare().getQueue();
             // 1 will follow discussion 1 and 2
@@ -80,28 +82,24 @@ public class RoutingTest {
             channel2.queueBind(queue2, EXCHANGE_NAME, CONVERSATION_2);
 
             String queue3 = channel3.queueDeclare().getQueue();
-            // 1 will follow discussion 3 and 4
+            // 3 will follow discussion 3 and 4
             channel3.queueBind(queue3, EXCHANGE_NAME, CONVERSATION_3);
             channel3.queueBind(queue3, EXCHANGE_NAME, CONVERSATION_4);
 
             String queue4 = channel4.queueDeclare().getQueue();
-            // 1 will follow discussion 1 and 2
+            // 4 will follow discussion 1 and 4
             channel4.queueBind(queue4, EXCHANGE_NAME, CONVERSATION_1);
             channel4.queueBind(queue4, EXCHANGE_NAME, CONVERSATION_4);
 
-            // 1 will produce 100 messages
-            String message1 = "1";
+            // Publish one message on each discussion
             channel1.basicPublish(EXCHANGE_NAME, CONVERSATION_1, NO_PROPERTIES,
-                message1.getBytes(StandardCharsets.UTF_8));
-            String message2 = "2";
+                "1".getBytes(StandardCharsets.UTF_8));
             channel2.basicPublish(EXCHANGE_NAME, CONVERSATION_2, NO_PROPERTIES,
-                message2.getBytes(StandardCharsets.UTF_8));
-            String message3 = "3";
+                "2".getBytes(StandardCharsets.UTF_8));
             channel3.basicPublish(EXCHANGE_NAME, CONVERSATION_3, NO_PROPERTIES,
-                message3.getBytes(StandardCharsets.UTF_8));
-            String message4 = "4";
+                "3".getBytes(StandardCharsets.UTF_8));
             channel4.basicPublish(EXCHANGE_NAME, CONVERSATION_4, NO_PROPERTIES,
-                message4.getBytes(StandardCharsets.UTF_8));
+                "4".getBytes(StandardCharsets.UTF_8));
 
             InMemoryConsumer consumer1 = new InMemoryConsumer(channel1);
             InMemoryConsumer consumer2 = new InMemoryConsumer(channel2);
@@ -111,12 +109,24 @@ public class RoutingTest {
             channel2.basicConsume(queue2, consumer2);
             channel3.basicConsume(queue3, consumer3);
             channel4.basicConsume(queue4, consumer4);
-            Thread.sleep(1000 * 5);
 
-           assertThat(consumer1.getConsumedMessages()).containsOnly(1, 2);
-           assertThat(consumer2.getConsumedMessages()).containsOnly(2, 3);
-           assertThat(consumer3.getConsumedMessages()).containsOnly(3, 4);
-           assertThat(consumer4.getConsumedMessages()).containsOnly(1, 4);
+            awaitAtMostOneMinute.until(() -> allMessageReceived(consumer1, consumer2, consumer3, consumer4));
+
+            assertThat(consumer1.getConsumedMessages()).containsOnly(1, 2);
+            assertThat(consumer2.getConsumedMessages()).containsOnly(2, 3);
+            assertThat(consumer3.getConsumedMessages()).containsOnly(3, 4);
+            assertThat(consumer4.getConsumedMessages()).containsOnly(1, 4);
         }
+    }
+
+    private boolean allMessageReceived(InMemoryConsumer consumer1, InMemoryConsumer consumer2,
+                                       InMemoryConsumer consumer3, InMemoryConsumer consumer4) {
+        return Iterables.size(
+            Iterables.concat(
+                consumer1.getConsumedMessages(),
+                consumer2.getConsumedMessages(),
+                consumer3.getConsumedMessages(),
+                consumer4.getConsumedMessages()))
+            == 8;
     }
 }
