21b8827cc1c84853f0587a0897a427b8d9cc581e
benwa <btellier@linagora.com>
2018-02-09 09:30:39 +0700

    fixup! fixup! JAMES-2334 Demonstrate that broadcast is working

--- server/queue/queue-rabbitmq/src/test/java/org/apache/james/queue/rabbitmq/BroadcastTest.java
+++ server/queue/queue-rabbitmq/src/test/java/org/apache/james/queue/rabbitmq/BroadcastTest.java
@@ -19,20 +19,27 @@
 
 package org.apache.james.queue.rabbitmq;
 
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.DIRECT;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.DURABLE;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.EXCHANGE_NAME;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.NO_PROPERTIES;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.ROUTING_KEY;
+import static org.apache.james.queue.rabbitmq.RabbitMQFixture.awaitAtMostOneMinute;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import com.github.fge.lambdas.Throwing;
 import com.github.steveash.guavate.Guavate;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -43,12 +50,6 @@ import com.rabbitmq.client.Envelope;
 @ExtendWith(DockerRabbitMQExtension.class)
 public class BroadcastTest {
 
-    private static final String EXCHANGE_NAME = "exchangeName";
-    private static final boolean DURABLE = true;
-    private static final String ROUTING_KEY = "";
-    private static final AMQP.BasicProperties NO_PROPERTIES = null;
-    private static final long ONE_MINUTE = TimeUnit.MINUTES.toMillis(1);
-
     private ConnectionFactory connectionFactory1;
     private ConnectionFactory connectionFactory2;
     private ConnectionFactory connectionFactory3;
@@ -56,42 +57,34 @@ public class BroadcastTest {
 
     @BeforeEach
     public void setup(DockerRabbitMQ rabbitMQ) {
-        connectionFactory1 = prepareConnectionFactory(rabbitMQ);
-        connectionFactory2 = prepareConnectionFactory(rabbitMQ);
-        connectionFactory3 = prepareConnectionFactory(rabbitMQ);
-        connectionFactory4 = prepareConnectionFactory(rabbitMQ);
+        connectionFactory1 = rabbitMQ.connectionFactory();
+        connectionFactory2 = rabbitMQ.connectionFactory();
+        connectionFactory3 = rabbitMQ.connectionFactory();
+        connectionFactory4 = rabbitMQ.connectionFactory();
     }
 
-    private ConnectionFactory prepareConnectionFactory(DockerRabbitMQ rabbitMQ) {
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost(rabbitMQ.getHostIp());
-        connectionFactory.setPort(rabbitMQ.getPort());
-        connectionFactory.setUsername(rabbitMQ.getUsername());
-        connectionFactory.setPassword(rabbitMQ.getPassword());
-        return connectionFactory;
-    }
-
-    @Test
     // In the following case, each consumer will receive the messages produced by the
     // producer
 
     // To do so, each consumer will bind it's queue to the producer exchange.
+    @Test
     public void rabbitMQShouldSupportTheBroadcastCase() throws Exception {
+        ImmutableList<Integer> expectedResult = IntStream.range(0, 10).boxed().collect(Guavate.toImmutableList());
         ConcurrentLinkedQueue<Integer> results2 = new ConcurrentLinkedQueue<>();
         ConcurrentLinkedQueue<Integer> results3 = new ConcurrentLinkedQueue<>();
         ConcurrentLinkedQueue<Integer> results4 = new ConcurrentLinkedQueue<>();
 
         try (Connection connection1 = connectionFactory1.newConnection();
              Channel publisherChannel = connection1.createChannel();
-            Connection connection2 = connectionFactory2.newConnection();
+             Connection connection2 = connectionFactory2.newConnection();
              Channel subscriberChannel2 = connection2.createChannel();
-            Connection connection3 = connectionFactory3.newConnection();
+             Connection connection3 = connectionFactory3.newConnection();
              Channel subscriberChannel3 = connection3.createChannel();
-            Connection connection4 = connectionFactory4.newConnection();
+             Connection connection4 = connectionFactory4.newConnection();
              Channel subscriberChannel4 = connection4.createChannel()) {
 
             // Declare the a single exchange and three queues attached to it.
-            publisherChannel.exchangeDeclare(EXCHANGE_NAME, "direct", DURABLE);
+            publisherChannel.exchangeDeclare(EXCHANGE_NAME, DIRECT, DURABLE);
 
             String queue2 = subscriberChannel2.queueDeclare().getQueue();
             subscriberChannel2.queueBind(queue2, EXCHANGE_NAME, ROUTING_KEY);
@@ -105,19 +98,25 @@ public class BroadcastTest {
             subscriberChannel4.basicConsume(queue4, storeInResultCallBack(subscriberChannel4, results4));
 
             // the publisher will produce 10 messages
-            for (int i = 0; i < 10; i++) {
-                publisherChannel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, NO_PROPERTIES,
-                    String.valueOf(i).getBytes(StandardCharsets.UTF_8));
-            }
+            expectedResult.stream()
+                .map(String::valueOf)
+                .map(s -> s.getBytes(StandardCharsets.UTF_8))
+                .forEach(Throwing.consumer(
+                    bytes -> publisherChannel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, NO_PROPERTIES, bytes)));
 
-            Thread.sleep(ONE_MINUTE);
+            awaitAtMostOneMinute.until(() -> allMessageReceived(expectedResult, results2, results3, results4));
 
             // Check every subscriber have receive all the messages.
-            ImmutableList<Integer> expectedResult = IntStream.range(0, 10).boxed().collect(Guavate.toImmutableList());
             assertThat(results2).containsOnlyElementsOf(expectedResult);
             assertThat(results3).containsOnlyElementsOf(expectedResult);
             assertThat(results4).containsOnlyElementsOf(expectedResult);
         }
+    }
+
+    private boolean allMessageReceived(ImmutableList<Integer> expectedResult, ConcurrentLinkedQueue<Integer> results2, ConcurrentLinkedQueue<Integer> results3, ConcurrentLinkedQueue<Integer> results4) {
+        return Iterables.size(
+            Iterables.concat(results2, results3, results4))
+            == expectedResult.size() * 3;
     }
 
     private DefaultConsumer storeInResultCallBack(Channel channel, ConcurrentLinkedQueue<Integer> results) {
