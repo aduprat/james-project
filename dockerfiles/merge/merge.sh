faade88b56fdb010920cf2b7fe1c4e6869885a86
Antoine Duprat <aduprat@linagora.com>
2018-10-11 10:11:42 +0200

    fixup! JAMES-2559 Add original branch parameter in merging tool

--- dockerfiles/merge/merge.sh
+++ dockerfiles/merge/merge.sh
@@ -2,35 +2,39 @@
 
 printUsage() {
    echo "Usage : "
-   echo "./merge.sh SHA1 RESULTING_BRANCH"
-   echo "    BRANCH: the original branch used for merge"
+   echo "./merge.sh SHA1 RESULTING_BRANCH ORIGINAL_BRANCH"
    echo "    SHA1: SHA1 to merge with the branch"
    echo "    RESULTING_BRANCH : Resulting branch of the merge"
+   echo "    ORIGINAL_BRANCH: the original branch used for merge (if none, then `master` will be used)"
    exit 1
 }
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
     printUsage
 fi
 
-BRANCH=$1
-SHA1=$2
-RESULTING_BRANCH=$3
+SHA1=$1
+RESULTING_BRANCH=$2
+if [ "$#" -eq 3 ]; then
+    ORIGINAL_BRANCH=$3
+else
+    ORIGINAL_BRANCH=master
+fi
 
 APACHE_REPO=`git remote show | grep apache || true`
 if [ -z "$APACHE_REPO" ]; then
     git remote add apache https://github.com/apache/james-project.git
 fi 
-# Getting BRANCH from apache repo
+# Getting original branch from apache repo
 git fetch apache
-git checkout apache/$BRANCH -B $BRANCH
+git checkout apache/$ORIGINAL_BRANCH -B $ORIGINAL_BRANCH
 
 # Getting the branch to be merged from /origin
 git fetch origin
 git checkout $SHA1
 git checkout -b SHA1_BRANCH
 
-# Merging the branch to be merged in the original BRANCH
-git checkout $BRANCH
+# Merging the branch to be merged in the original branch
+git checkout $ORIGINAL_BRANCH
 git checkout -b $RESULTING_BRANCH
 git merge --no-edit SHA1_BRANCH
