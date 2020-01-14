f28b1a31b1e8e0da15f6fb406a8f5ed57e1e4adb
Antoine DUPRAT <aduprat@linagora.com>
2020-01-14 09:36:35 +0100

    Introduce buid on Github PRs

---
+++ ci/GithubPR.groovy
@@ -0,0 +1,28 @@
+pipelineJob('James-PR') {
+  properties {
+    githubProjectUrl('https://github.com/aduprat/james-project')
+  }
+  triggers {
+    githubPullRequest {
+      orgWhitelist('linagora')
+      cron ('* * * * *')
+      triggerPhrase('test this please')
+      allowMembersOfWhitelistedOrgsAsAdmin()
+    }
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/aduprat/james-project')
+            name('origin')
+            refspec('+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*')
+          }
+          branch('${sha1}')
+        }
+      }
+      lightweight(false)
+    }
+  }
+}
